5762d882ad4e4606b3a0bfca74723100fc2ee68b
Søren Andersen <sgan@stibosystems.com>
2017-03-13 22:34:57 +0100

    Edited Main for REST

--- REST/src/com/stibosystems/restdemo/Main.java
+++ REST/src/com/stibosystems/restdemo/Main.java
@@ -7,8 +7,12 @@ import javax.sound.midi.Soundbank;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        RESTClient restClient = new RESTClient(args[0], args[1], args[2]);
-        for(BGPImportStatus bgpImportStatus : restClient.getIEPBackgroundProcessIDs("AssetAutoImport")) {
+        String systemName = args[0];
+        String userName = args[1];
+        String password = args[2];
+        String iepID = args[3];
+        RESTClient restClient = new RESTClient(systemName, userName, password);
+        for(BGPImportStatus bgpImportStatus : restClient.getIEPBackgroundProcessIDs(iepID)) {
             System.out.println("(" + bgpImportStatus.type + ") - Status on import of file " + restClient.getImportFileNameFromBGPExecutionReport(bgpImportStatus.bgpID) + ", status " + bgpImportStatus.status + ", progress " + bgpImportStatus.progess + "%, (BGP ID: " + bgpImportStatus.bgpID + ")") ;
         }
     }
